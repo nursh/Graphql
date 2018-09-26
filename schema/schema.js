f489d18d028fac22f87900e06b25749945ac7f76
nursh <nsheikhmohammed@yahoo.com>
2018-09-26 15:35:27 -0400

    Created schema and added it to the graphql middleware

---
+++ schema/schema.js
@@ -0,0 +1,40 @@
+const graphql = require('graphql');
+const _ = require('lodash');
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema,
+} = graphql;
+
+
+const users = [
+  { id: '20', firstName: 'Johnny', age: 20 },
+  { id: '22', firstName: 'Michael', age: 25 }
+];
+
+const UserType = new GraphQLObjectType({
+  name: 'User',
+  fields: {
+    id: { type: GraphQLString },
+    firstName: { type: GraphQLString },
+    age: { type: GraphQLInt },
+  }
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return _.find(users, { id: args.id });
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+});
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const expressGraphQL = require('express-graphql');
+const schema = require('./schema/schema');
 
 const app = express();
 const port = process.env.PORT || 4000;
 
 app.use('/graphql', expressGraphQL({
+  schema,
   graphiql: true,
 }));
 
