158d82a5ecbe01b2736dba5cb2ac5bc0d2067955
nursh <nsheikhmohammed@yahoo.com>
2018-09-26 09:07:17 -0400

    Created schema file to hold schema for database

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const expressGraphQL = require('express-graphql');
 
 const app = express();
+const port = process.env.PORT || 4000;
 
-app.listen(4000, () => console.log(`App server is running on port:${4000}`));
+app.use('/graphql', expressGraphQL({
+  graphiql: true,
+}));
+
+app.listen(port, () => console.log(`App server is running on port:${port}`));
