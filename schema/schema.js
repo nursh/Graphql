5c43adea22b9aaaf84012ed1361a1a733f7484ee
nursh <nsheikhmohammed@yahoo.com>
2018-09-29 08:23:04 -0400

    Added query to get users from a company

--- schema/schema.js
+++ schema/schema.js
@@ -5,20 +5,28 @@ const {
   GraphQLString,
   GraphQLInt,
   GraphQLSchema,
+  GraphQLList,
 } = graphql;
 
 const CompanyType = new GraphQLObjectType({
   name: 'Company',
-  fields: {
+  fields: () => ({
     id: { type: GraphQLString },
     name: { type: GraphQLString },
     description: { type: GraphQLString },
-  },
+    users: {
+      type: new GraphQLList(UserType),
+      async resolve(parentValue, args) {
+        const response = await axios.get(`http://localhost:3000/companies/${parentValue.id}/users`);
+        return response.data;
+      }
+    }
+  }),
 });
 
 const UserType = new GraphQLObjectType({
   name: 'User',
-  fields: {
+  fields: () => ({
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
@@ -29,7 +37,7 @@ const UserType = new GraphQLObjectType({
         return response.data;
       }
     },
-  }
+  })
 });
 
 const RootQuery = new GraphQLObjectType({
@@ -40,6 +48,14 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLString } },
       async resolve(parentValue, args) {
         const response = await axios.get(`http://localhost:3000/users/${args.id}`);
+        return response.data;
+      }
+    },
+    company: {
+      type: CompanyType,
+      args: { id: { type: GraphQLString } },
+      async resolve(parentValue, args) {
+        const response = await axios.get(`http://localhost:3000/companies/${args.id}`);
         return response.data;
       }
     }
